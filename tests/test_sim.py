afc929d7d07fad4a4414e81e05b0b9f60ee2984f
Robyn Stuart <robyna.s@gmail.com>
2025-11-17 15:11:17 +1100

    move partnerships

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -14,7 +14,7 @@ parallel = 1  # Whether to run in serial (for debugging)
 
 def test_simple():
     sc.heading('Test simplest possible FPsim run')
-    sim = fp.Sim(test=True)
+    sim = fp.Sim(test=True, use_partnership=True)
     sim.run()
     sim.to_df(resample='year', use_years=True)
     return sim
