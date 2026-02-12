047d020c59c6b7727d93554318175dff51d728cb
Robyn Stuart <robyna.s@gmail.com>
2026-02-11 20:15:28 -0800

    v

--- fpsim/version.py
+++ fpsim/version.py
@@ -1,3 +1,3 @@
-__version__ = '3.5.0'
-__versiondate__ = '2026-02-10'
+__version__ = '3.6.0'
+__versiondate__ = '2026-02-XX'
 __license__ = f'FPsim {__version__} ({__versiondate__}) — © 2019-2026 by IDM'
