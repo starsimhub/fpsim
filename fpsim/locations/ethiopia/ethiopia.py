23c8121ca21ab7d52bc6a063b37afcf97c049913
RHull-IDM <ryan.hull@gatesfoundation.org>
2026-03-17 17:06:17 -0700

    more updates to ethiopia calib

--- fpsim/locations/ethiopia/ethiopia.py
+++ fpsim/locations/ethiopia/ethiopia.py
@@ -10,12 +10,14 @@ import fpsim.locations.data_utils as fpld
 def make_calib_pars():
     """ Make a dictionary of location-specific parameters """
     pars = {}
-    pars['exposure_factor'] = 1.1
-    pars['prob_use_intercept'] = -0.8657
+    pars['exposure_factor'] = 1.2
+    pars['prob_use_intercept'] = -1.0
+    pars['prob_use_trend_par'] = 0.04
     pars['fecundity_low'] = 0.6498
     pars['fecundity_high'] = 1.7560
+    pars['method_weights'] = np.array([0.2, 0.2, 100, 1, 0.15, 0.5, 0.05, 0.5, 1])
     pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
-                                      [1.0, 0.4854, 0.4431, 0.35, 1.1, 0.9, 0.95, 1.0, 0.8, 0.8, 0.6, 0.2, 0.1]])
+                                      [1.0, 0.4854, 0.4431, 0.35, 1.0, 0.9, 0.95, 1.0, 0.9, 0.8, 0.5, 0.25, 0.1]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
                                         [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]])
     pars['dur_postpartum'] = 18
