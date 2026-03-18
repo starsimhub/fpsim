starsimhub/fpsim

commit 43d372c1d563d0958268ee73a41dcfe070654183

updated parameter merging to use recursive merging which allows partial dictionary specification matching, and tidied up location .py files to take advantage

fpsim/sim.py
@@ -200,7 +200,7 @@ class Sim(ss.Sim):
         calib_pars = fpd.get_calib_pars(location, verbose=verbose)
         if calib_pars is not None:
             sc.printv(f'Applying calibration parameters for {location}...', veps)
-            all_pars = sc.mergedicts(calib_pars, all_pars)
+            all_pars = fp.mergepars(all_pars, calib_pars)  # Use smart merging for calibration parameters
 
         # Deal with all module pars in a loop
         module_par_map = {
@@ -215,7 +215,7 @@ class Sim(ss.Sim):
             indirect_module_pars = {k: v for k, v in all_pars.items() if k in module_default_pars.keys()}  # From pars or kwargs
             for k in indirect_module_pars: all_pars.pop(k)
             data_module_pars = data_dict.get(module, {})
-            merged_pars = sc.mergedicts(data_module_pars, indirect_module_pars, direct_user_pars, _copy=True)
+            merged_pars = fp.mergepars(data_module_pars, indirect_module_pars, direct_user_pars, _copy=True)
             setattr(self, f'{module}_pars', merged_pars)
 
         # Raise an exception if there are any leftover pars

fpsim/locations/cotedivoire/cotedivoire.py
@@ -7,36 +7,6 @@ import pandas as pd
 import starsim as ss
 import fpsim.locations.data_utils as fpld
 
-# def birth_spacing_pref():
-#     """
-#     Returns an array of birth spacing preferences by closest postpartum month.
-#     If the CSV file is missing, a default table with equal weights is used.
-#     """
-#     # Try to read the CSV, fallback to dummy df if not found
-#     try:
-#         df = fpld.read_data('birth_spacing_pref.csv', os.path.abspath(__file__) / 'data' )
-#     except FileNotFoundError:
-#         print(f"birth_spacing_pref.csv not found, using default weights of 1.")
-#         months = np.arange(0, 39, 3)  # 0 to 36 months in 3-month intervals
-#         weights = np.ones_like(months, dtype=float)
-#         df = pd.DataFrame({'month': months, 'weights': weights})
-#
-#     # Check uniform intervals
-#     intervals = np.diff(df['month'].values)
-#     interval = intervals[0]
-#     assert np.all(intervals == interval), (
-#         f"In order to be computed in an array, birth spacing preference bins must be uniform. Got: {intervals}"
-#     )
-#
-#     pref_spacing = {
-#         'interval': interval,
-#         'n_bins': len(intervals),
-#         'months': df['month'].values,
-#         'preference': df['weights'].values
-#     }
-#
-#     return pref_spacing
-
 def make_calib_pars():
     pars = {}
     pars['fecundity'] = ss.uniform(low=0.7, high=1.1)
@@ -47,15 +17,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([8, 5, 5, 20, 2, 2, 0.015, 0.015, 1])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)  # 0 to 51 months in 3-month intervals
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1  # Spacing of 0-6 months
-    pars['spacing_pref']['preference'][3:6] = 0.5  # Spacing of 9-15 months
-    pars['spacing_pref']['preference'][6:9] = 0.8  # Spacing of 18-24 months
-    pars['spacing_pref']['preference'][9:] =  2  # Spacing of 27-36 months
+    spacing_pref_array = np.ones(13, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1     # Spacing of 0-6 months
+    spacing_pref_array[3:6] = 0.5   # Spacing of 9-15 months  
+    spacing_pref_array[6:9] = 0.8   # Spacing of 18-24 months
+    spacing_pref_array[9:] =  2     # Spacing of 27+ months
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0,    5,  10, 12.5, 15, 18, 20, 25,  30,  35, 40, 45, 50],
                                         [0.1, 0.1, 0.5,  0.1,0.1, 0.6,1.5,0.6,0.6,0.2,  0.3,0.5,0.2]])  # <<< USER-EDITABLE: Can be modified for calibration
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/locations/niger/niger.py
@@ -15,15 +15,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([0.005, 0.012, 5, 0.5, 0.8, 0.5, 0.001, 0.05, 50])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1
-    pars['spacing_pref']['preference'][3:6] = 1
-    pars['spacing_pref']['preference'][6:9] = 2
-    pars['spacing_pref']['preference'][9:] =  0.2
+    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1
+    spacing_pref_array[3:6] = 1
+    spacing_pref_array[6:9] = 2
+    spacing_pref_array[9:] =  0.2
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0,    5,  10, 12.5, 15, 18, 20, 25,  30,  35, 40, 45, 50],
                                         [0.5, 0.5, 0.5, 0.6, 0.6, 0.9,  0.9,  1,  0.8,0.4,0.4,0.4,0.3]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/locations/nigeria_kaduna/nigeria_kaduna.py
@@ -15,15 +15,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([0.005, 0.005, 200, 10, 1, 0.001, 0.003, 10, 100])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1
-    pars['spacing_pref']['preference'][3:6] = 0.5
-    pars['spacing_pref']['preference'][6:9] = 0.8
-    pars['spacing_pref']['preference'][9:] =  2
+    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1
+    spacing_pref_array[3:6] = 0.5
+    spacing_pref_array[6:9] = 0.8
+    spacing_pref_array[9:] =  2
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0,     5,  10, 12.5, 15, 18, 20, 25,  30, 35,  40, 45,    50],
                                         [0.1, 0.1, 0.5,  3,   2,  0.5,0.5,1.5, 1.5, 1,   1,  1,   0.5]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/locations/nigeria_kano/nigeria_kano.py
@@ -15,15 +15,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([0.0012, 0.07, 3.5, 0.4, 0.5, 0.1, 300, 7, 3])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1
-    pars['spacing_pref']['preference'][3:6] = 1
-    pars['spacing_pref']['preference'][6:9] = 1
-    pars['spacing_pref']['preference'][9:] =  1
+    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1
+    spacing_pref_array[3:6] = 1
+    spacing_pref_array[6:9] = 1
+    spacing_pref_array[9:] =  1
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0,     5, 10, 12.5,   15, 18, 20,  25, 30,   35, 40, 45, 50],
                                         [0.5, 0.5, 0.5, 0.4 , 0.5,  1,  1,  1.1,1.7,  1.7, 0.8,0.5,0.5]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/locations/nigeria_lagos/nigeria_lagos.py
@@ -15,15 +15,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([0.2, 0.2, 0.1, 0.2, 30, 0.5, 1, 50, 5])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1
-    pars['spacing_pref']['preference'][3:6] = 0.5
-    pars['spacing_pref']['preference'][6:9] = 0.8
-    pars['spacing_pref']['preference'][9:] =  2
+    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1
+    spacing_pref_array[3:6] = 0.5
+    spacing_pref_array[6:9] = 0.8
+    spacing_pref_array[9:] =  2
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30,  35, 40, 45,  50],
                                         [1, 1, 1,  2,    2 ,0.3,0.8,1.3,0.4,0.6,0.3,0.4, 0.5]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/locations/pakistan_sindh/pakistan_sindh.py
@@ -15,15 +15,15 @@ def make_calib_pars():
     pars['method_weights'] = np.array([0.06, 2.3, 20, 2, 2.5, 1.5, 3, 0.1, 0.01])
     pars['dur_postpartum'] = 23
 
-    pars['spacing_pref'] = {}
-    pars['spacing_pref']['interval'] = 3.0
-    pars['spacing_pref']['n_bins'] = 18
-    pars['spacing_pref']['months'] = np.arange(0, 54, 3)
-    pars['spacing_pref']['preference'] = np.ones(pars['spacing_pref']['n_bins'], dtype=float)
-    pars['spacing_pref']['preference'][:3] =  1
-    pars['spacing_pref']['preference'][3:6] = 1
-    pars['spacing_pref']['preference'][6:9] = 5
-    pars['spacing_pref']['preference'][9:] =  0.1
+    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
+    spacing_pref_array[:3] =  1
+    spacing_pref_array[3:6] = 1
+    spacing_pref_array[6:9] = 5
+    spacing_pref_array[9:] =  0.1
+    
+    pars['spacing_pref'] = {
+        'preference': spacing_pref_array
+    }
     pars['exposure_age'] = np.array([[0,     5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
                                         [0.2, 0.2, 0.2,  1 ,0.9, 0.8,0.8,1.25,1, 0.7,0.5,0.5, 0.5]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],

fpsim/parameters.py
@@ -128,16 +128,47 @@ def make_fp_pars(location=None):
     return FPPars(location=location)
 
 
-def mergepars(*args):
+def mergepars(*args, _copy=False, **kwargs):
     """
-    Merge all parameter dictionaries into a single dictionary.
+    Merge all parameter dictionaries into a single dictionary with nested merging.
     This is used to initialize the SimPars class with all relevant parameters.
-    It wraps the sc.mergedicts function to ensure all inputs are dicts
+    
+    Unlike sc.mergedicts, this function recursively merges nested dictionaries instead
+    of replacing them entirely. This allows partial dictionary specifications in
+    calibration parameters (e.g., only specifying preference values in spacing_pref
+    while preserving interval, n_bins, months from defaults).
+    
+    Args:
+        _copy (bool): whether to deep copy the input dictionaries (default False, same as sc.mergedicts)
+        *args: dictionaries to merge
+        **kwargs: additional parameters (for compatibility with sc.mergedicts)
     """
-    # Convert any Pars objects to plain dicts and merge
-    dicts = [dict(sc.dcp(arg)) for arg in args if arg is not None]
-    merged_pars = sc.mergedicts(*dicts)
-    return merged_pars
+    # Convert any Pars objects to plain dicts
+    if _copy:
+        dicts = [dict(sc.dcp(arg)) for arg in args if arg is not None]
+    else:
+        dicts = [dict(arg) if arg is not None else {} for arg in args if arg is not None]
+    
+    if len(dicts) < 2:
+        # Single dict or empty - use standard merging
+        return sc.mergedicts(*dicts, _copy=_copy, **kwargs) if dicts else {}
+    
+    # Start with the first dictionary
+    result = sc.dcp(dicts[0]) if _copy else dicts[0]
+    
+    # Recursively merge each subsequent dictionary
+    for next_dict in dicts[1:]:
+        for key, value in next_dict.items():
+            if (key in result and 
+                isinstance(value, dict) and 
+                isinstance(result[key], dict)):
+                # Both are dicts - recursively merge
+                result[key] = mergepars(result[key], value, _copy=_copy, **kwargs)
+            else:
+                # Not both dicts - use the new value
+                result[key] = sc.dcp(value) if _copy else value
+    
+    return result
 
 
 # Shortcut for accessing default keys

commit 589d49dc6bb37e4266d7ef44d5c0fdc03c992c93

Fix calib pars overriding user-provided pars in mergepars
The switch from sc.mergedicts to fp.mergepars in 43d372c1 accidentally
flipped the argument order, giving calibration pars higher priority than
user-specified pars. This was latent until calibration dicts gained scalar
pars like exposure_factor and prob_use_intercept.

Also fix test_change_par assertion to compare against actual original value
instead of hardcoded 1.0, and fix crash in its error message.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

fpsim/sim.py
@@ -215,7 +215,7 @@ class Sim(ss.Sim):
         calib_pars = fpd.get_calib_pars(location, verbose=verbose)
         if calib_pars is not None:
             sc.printv(f'Applying calibration parameters for {location}...', veps)
-            all_pars = fp.mergepars(all_pars, calib_pars)  # Use smart merging for calibration parameters
+            all_pars = fp.mergepars(calib_pars, all_pars)  # User pars override calibration pars
 
         # Deal with all module pars in a loop
         module_par_map = {

tests/test_interventions.py
@@ -63,7 +63,8 @@ def test_change_par():
     assert s1.pars.fp.exposure_factor == ec, f'change_pars() did not change exposure factor to {ec}'
     assert cp1_births < base_births, f'Reducing exposure factor should reduce births, but {cp1_births} is not less than the baseline of {base_births}'
 
-    assert s2.pars.fp.exposure_factor == 1.0, f'Exposure factor should be reset back to 1.0, but it is {s2["exposure_factor"]}'
+    orig_ef = s0.pars.fp.exposure_factor
+    assert s2.pars.fp.exposure_factor == orig_ef, f'Exposure factor should be reset back to {orig_ef}, but it is {s2.pars.fp.exposure_factor}'
     assert cp2_births <= base_births, f'Reducing exposure factor temporarily should reduce births, but {cp2_births} is not less than the baseline of {base_births}'
 
     return m