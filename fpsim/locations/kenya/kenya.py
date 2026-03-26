174d8adc0ff4deed7275ab03421543b58112b5eb
Robyn Stuart <robyna.s@gmail.com>
2026-03-26 13:06:33 -0700

    Merge rc3.5.3 calibration updates and recalibrate all locations
    Brings in calibration infrastructure from rc3.5.3 (PR #712) and
    recalibrates all 9 locations for the v3.6 architecture (200 trials each).
    
    Locations calibrated: senegal (4.10), kenya (3.50), cotedivoire (4.15),
    ethiopia (4.33), niger (4.79), nigeria_kaduna (4.93), nigeria_kano (4.65),
    nigeria_lagos (5.87), pakistan_sindh (4.23).

--- fpsim/locations/kenya/kenya.py
+++ fpsim/locations/kenya/kenya.py
@@ -2,26 +2,24 @@
 Set the parameters for a location-specific FPsim model.
 """
 import numpy as np
-from fpsim import defaults as fpd
-import fpsim as fp
 import fpsim.locations.data_utils as fpld
 
 
 def make_calib_pars():
-    """ Make a dictionary of location-specific parameters """
+    """ Make a dictionary of location-specific parameters (mismatch: 3.50) """
     pars = {}
-
-    # FP parameters - move to csv?
-    pars['exposure_age'] = np.array([
-                [0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
-                [1, 1, 1,  1 ,   .4, 1, 1. ,.8, .8, .5, .3, .5, .5]
-    ])
-
-    pars['exposure_parity'] = np.array([
-                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
-                [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]
-    ])
-
+    pars['exposure_factor'] = 0.5382
+    pars['prob_use_intercept'] = -2.2959
+    pars['prob_use_trend_par'] = 0.0429
+    pars['fecundity_low'] = 0.5237
+    pars['fecundity_high'] = 2.4048
+    pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
+                                      [1.0000, 0.2574, 0.4966, 0.3587, 1.9006, 1.6208, 1.1313, 1.7979, 2.3366, 1.8868, 0.6719, 0.0974, 0.4532]])
+    pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
+                                         [1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 0.9175, 0.4272, 0.2851, 0.2592, 0.1574, 0.0382, 0.0623]])
+    pars['spacing_pref'] = {
+        'preference': np.array([1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 0.9581, 0.9162, 0.8743, 0.8324, 0.7905, 0.7486, 0.7067, 0.6648])
+    }
     return pars
 
 
--- fpsim/locations/nigeria_kano/nigeria_kano.py
+++ fpsim/locations/nigeria_kano/nigeria_kano.py
@@ -1,35 +1,25 @@
 """
-Set the parameters for Nigeria Kano.
+Set the parameters for a location-specific FPsim model.
 """
 import numpy as np
-import starsim as ss
 import fpsim.locations.data_utils as fpld
 
+
 def make_calib_pars():
+    """ Make a dictionary of location-specific parameters (mismatch: 4.65) """
     pars = {}
-    pars['fecundity_low'] = 0.35
-    pars['fecundity_high'] = 1.1
-    pars['exposure_factor'] = 0.75
-    pars['prob_use_year'] = 2020
-    pars['prob_use_trend_par'] = 0.01
-    pars['prob_use_intercept'] = 1.2
-    pars['method_weights'] = np.array([0.0012, 0.07, 3.5, 0.4, 0.5, 0.1, 300, 7, 3])
-    pars['dur_postpartum'] = 23
-
-    spacing_pref_array = np.ones(18, dtype=float)  # Size based on n_bins from data files
-    spacing_pref_array[:3] =  1
-    spacing_pref_array[3:6] = 1
-    spacing_pref_array[6:9] = 1
-    spacing_pref_array[9:] =  1
-    
-    pars['spacing_pref'] = {
-        'preference': spacing_pref_array
-    }
-    pars['exposure_age'] = np.array([[0,     5, 10, 12.5,   15, 18, 20,  25, 30,   35, 40, 45, 50],
-                                        [0.5, 0.5, 0.5, 0.4 , 0.5,  1,  1,  1.1,1.7,  1.7, 0.8,0.5,0.5]])
+    pars['exposure_factor'] = 0.5543
+    pars['prob_use_intercept'] = -0.4945
+    pars['prob_use_trend_par'] = 0.0355
+    pars['fecundity_low'] = 0.8390
+    pars['fecundity_high'] = 1.3105
+    pars['exposure_age'] = np.array([[0, 5, 10, 12.5, 15, 18, 20, 25, 30, 35, 40, 45, 50],
+                                      [1.0000, 0.4729, 0.4282, 0.5124, 1.2211, 1.8405, 1.5522, 1.0511, 1.2528, 1.1136, 0.4651, 0.1810, 0.2428]])
     pars['exposure_parity'] = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20],
-                                           [1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.15, 0.10, 0.05, 0.01]])
-
+                                         [1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 0.8253, 0.7074, 0.4202, 0.3672, 0.0402, 0.1769, 0.0723]])
+    pars['spacing_pref'] = {
+        'preference': np.array([1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 0.9097, 0.8193, 0.7290, 0.6387, 0.5483, 0.4580, 0.3677])
+    }
     return pars
 
 
